a02501fc80e5919a53fcf2953e5682ad65164506
Abhishek Wasave <Abhi_wasave@abw12>
2024-02-20 22:42:08 +0530

    helm chart files added

--- src/test/java/com/abw12/absolutefitness/usermgmt/UserMgmtApplicationTests.java
+++ src/test/java/com/abw12/absolutefitness/usermgmt/UserMgmtApplicationTests.java
@@ -1,13 +1,13 @@
-package com.abw12.absolutefitness.usermgmt;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class UserMgmtApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.abw12.absolutefitness.usermgmt;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class UserMgmtApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
